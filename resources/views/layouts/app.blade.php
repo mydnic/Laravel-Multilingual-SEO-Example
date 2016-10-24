478875e4da12950a34c032dd7ce01c7b6354c5d8
Clément Rigo <rigoclement@mydnic.be>
2016-10-24 15:19:43 +0200

    fix carret

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
                     @endif
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            {{ app()->getLocale() }} <span class="carret"></span>
+                            {{ app()->getLocale() }} <i class="fa fa-caret-down"></i>
                         </a>
                         <ul class="dropdown-menu">
                             @foreach (config('translatable.locales') as $lang => $language)
